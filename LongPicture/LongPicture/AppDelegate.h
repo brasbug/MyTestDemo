6e50be4a3a8423982687a54fff566b3255533e2c
Jack <erfeng.cheng@dianping.com>
2015-11-05 10:28:30 +0800

    长图

---
+++ LongPicture/LongPicture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LongPicture
+//
+//  Created by Jack on 15/11/4.
+//  Copyright © 2015年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LongPicture/LongPicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LongPicture
+//
+//  Created by Jack on 15/11/4.
+//  Copyright © 2015年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
